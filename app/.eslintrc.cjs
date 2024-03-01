511fffcaacfed683342458c8fc48c1feaa2c89eb
MauricioPassosF <mauricio.passos97@gmail.com>
2024-03-01 14:01:01 -0300

    Configurando Lint

--- app/.eslintrc.cjs
+++ app/.eslintrc.cjs
@@ -2,10 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
+  parserOptions: {
+    project:'tsconfig.eslint.json',
+    tsconfigRootDir: './app',
+    sourceType: 'module' 
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
