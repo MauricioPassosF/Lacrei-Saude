d4bfc2600ed4da1548b67fa542de2f79500aa4a8
MauricioPassosF <mauricio.passos97@gmail.com>
2024-03-01 14:39:53 -0300

    React router instalado e funcionando

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,35 +1,21 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/react-in-jsx-scope */
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import {
+  Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
+} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Home />} />,
+    ),
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <RouterProvider router={router} />
   );
 }
 
